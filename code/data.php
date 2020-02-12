4ccaed09b2fc4f8c97b7b4fddb64af0397e2a493
EC2 Default User <ec2-user@ip-172-31-29-74.ec2.internal>
2020-02-12 12:25:46 +0000

    Try to change and comment

--- code/data.php
+++ code/data.php
@@ -1,5 +1,14 @@
 <?php
 
+//create a PHP object
+//1. create a static PHP object
+//2. create object from MySQL SELECT
+
+//last: custom select depending
+//on HTTP GET query
+
+//convert it to JSON
+
 echo "{\"productList\": []}";
 
 ?>
